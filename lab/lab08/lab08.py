4fef6ca3e12981a75a08693eefeae2ed3732405b
Wangjiahao <2328796228@qq.com>
2024-11-23 22:05:55 +0800

    finish lab8

--- lab/lab08/lab08.py
+++ lab/lab08/lab08.py
@@ -12,6 +12,16 @@ def cumulative_mul(t):
     Tree(5040, [Tree(60, [Tree(3), Tree(4), Tree(5)]), Tree(42, [Tree(7)])])
     """
     "*** YOUR CODE HERE ***"
+    t.label = product(t)
+    for i in t.branches:
+        cumulative_mul(i)
+
+
+def product(t):
+    result = t.label
+    for i in t.branches:
+        result *= product(i)
+    return result
 
 
 def prune_small(t, n):
@@ -31,12 +41,15 @@ def prune_small(t, n):
     >>> t3
     Tree(6, [Tree(1), Tree(3, [Tree(1), Tree(2)])])
     """
-    while ___________________________:
-        largest = max(_______________, key=____________________)
-        _________________________
-    for __ in _____________:
-        ___________________
+    while len(t.branches) > n:
+        largest = max(t.branches, key=label)
+        t.branches.remove(largest)
+    for i in t.branches:
+        prune_small(i, n)
 
+
+def label(t):
+    return t.label
 
 def delete(t, x):
     """Remove all nodes labeled x below the root within Tree t. When a non-leaf
@@ -58,13 +71,13 @@ def delete(t, x):
     Tree(1, [Tree(4), Tree(5), Tree(3, [Tree(6)]), Tree(6), Tree(7), Tree(8), Tree(4)])
     """
     new_branches = []
-    for _________ in ________________:
-        _______________________
+    for b in t.branches:
+        delete(b,x)
         if b.label == x:
-            __________________________________
+            new_branches.extend(b.branches)
         else:
-            __________________________________
-    t.branches = ___________________
+            new_branches.append(b)
+    t.branches = new_branches
 
 
 class Tree:
@@ -77,6 +90,7 @@ class Tree:
     >>> t.branches[1].is_leaf()
     True
     """
+
     def __init__(self, label, branches=[]):
         for b in branches:
             assert isinstance(b, Tree)
@@ -99,5 +113,5 @@ class Tree:
             for b in t.branches:
                 tree_str += print_tree(b, indent + 1)
             return tree_str
-        return print_tree(self).rstrip()
 
+        return print_tree(self).rstrip()
